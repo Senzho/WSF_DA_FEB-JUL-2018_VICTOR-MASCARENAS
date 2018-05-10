6937e8a03abab12b6768597dbc361e587b180724
Senzho <vijagama@outlook.es>
2018-05-10 03:02:36 -0500

    Modelos de datos
    Se agrega el modelo entidad relación además de una imágen, se crea la
    base de datos y se agrega el atributo Respuesta en la entidad Solicitud
    del modelo de dominio.

---
+++ Diseño/BD/WSN.sql
@@ -0,0 +1,147 @@
+-- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
+--
+-- Host: localhost    Database: WSN
+-- ------------------------------------------------------
+-- Server version	5.7.17-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `prestadorservicios`
+--
+
+DROP TABLE IF EXISTS `prestadorservicios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `prestadorservicios` (
+  `idPrestador` int(11) NOT NULL AUTO_INCREMENT,
+  `nombrePrestador` varchar(100) DEFAULT NULL,
+  `telefonoPrestador` varchar(10) DEFAULT NULL,
+  `correoPrestador` varchar(100) DEFAULT NULL,
+  `categoria` int(11) DEFAULT NULL,
+  `descripcionPrestador` text,
+  `fechaNacimiento` date DEFAULT NULL,
+  `idUsuario` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idPrestador`),
+  KEY `idUsuario` (`idUsuario`),
+  CONSTRAINT `prestadorservicios_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `prestadorservicios`
+--
+
+LOCK TABLES `prestadorservicios` WRITE;
+/*!40000 ALTER TABLE `prestadorservicios` DISABLE KEYS */;
+/*!40000 ALTER TABLE `prestadorservicios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `solicitante`
+--
+
+DROP TABLE IF EXISTS `solicitante`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `solicitante` (
+  `idSolicitante` int(11) NOT NULL AUTO_INCREMENT,
+  `nombreSolicitante` varchar(100) DEFAULT NULL,
+  `correoSolicitante` varchar(100) DEFAULT NULL,
+  `telefonoSolicitante` varchar(10) DEFAULT NULL,
+  `direccionSolicitante` text,
+  `fechaNacimiento` date DEFAULT NULL,
+  `idUsuario` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idSolicitante`),
+  KEY `idUsuario` (`idUsuario`),
+  CONSTRAINT `solicitante_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `solicitante`
+--
+
+LOCK TABLES `solicitante` WRITE;
+/*!40000 ALTER TABLE `solicitante` DISABLE KEYS */;
+/*!40000 ALTER TABLE `solicitante` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `solicitud`
+--
+
+DROP TABLE IF EXISTS `solicitud`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `solicitud` (
+  `idSolicitud` int(11) NOT NULL AUTO_INCREMENT,
+  `fechaRealizacion` date DEFAULT NULL,
+  `fecha` date DEFAULT NULL,
+  `fechaInicial` date DEFAULT NULL,
+  `estrellas` int(11) DEFAULT NULL,
+  `estado` int(11) DEFAULT NULL,
+  `comentario` text,
+  `respuesta` text,
+  `idSolicitante` int(11) DEFAULT NULL,
+  `idPrestador` int(11) DEFAULT NULL,
+  PRIMARY KEY (`idSolicitud`),
+  KEY `idSolicitante` (`idSolicitante`),
+  KEY `idPrestador` (`idPrestador`),
+  CONSTRAINT `solicitud_ibfk_1` FOREIGN KEY (`idSolicitante`) REFERENCES `solicitante` (`idSolicitante`),
+  CONSTRAINT `solicitud_ibfk_2` FOREIGN KEY (`idPrestador`) REFERENCES `prestadorservicios` (`idPrestador`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `solicitud`
+--
+
+LOCK TABLES `solicitud` WRITE;
+/*!40000 ALTER TABLE `solicitud` DISABLE KEYS */;
+/*!40000 ALTER TABLE `solicitud` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuario`
+--
+
+DROP TABLE IF EXISTS `usuario`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `usuario` (
+  `idUsuario` int(11) NOT NULL AUTO_INCREMENT,
+  `nombreUsuario` varchar(50) DEFAULT NULL,
+  `contrasena` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`idUsuario`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuario`
+--
+
+LOCK TABLES `usuario` WRITE;
+/*!40000 ALTER TABLE `usuario` DISABLE KEYS */;
+/*!40000 ALTER TABLE `usuario` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2018-05-10  2:59:57
