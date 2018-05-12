9cb1cb1bb902f90bb9f7c6d823141820856b62c1
Senzho <vijagama@outlook.es>
2018-05-12 01:09:33 -0500

    Descripciones de CU's
    Descripciones de casos de uso y correxiones a los modelos.

--- Diseño/BD/WSN.sql
+++ Diseño/BD/WSN.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
--- Host: localhost    Database: WSN
+-- Host: localhost    Database: wsn
 -- ------------------------------------------------------
 -- Server version	5.7.17-log
 
@@ -14,6 +14,32 @@
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `posicion`
+--
+
+DROP TABLE IF EXISTS `posicion`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `posicion` (
+  `idPosicion` int(11) NOT NULL AUTO_INCREMENT,
+  `tipo` int(11) DEFAULT NULL,
+  `idTipo` int(11) DEFAULT NULL,
+  `longitud` double DEFAULT NULL,
+  `latitud` double DEFAULT NULL,
+  PRIMARY KEY (`idPosicion`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `posicion`
+--
+
+LOCK TABLES `posicion` WRITE;
+/*!40000 ALTER TABLE `posicion` DISABLE KEYS */;
+/*!40000 ALTER TABLE `posicion` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `prestadorservicios`
@@ -31,6 +57,7 @@ CREATE TABLE `prestadorservicios` (
   `descripcionPrestador` text,
   `fechaNacimiento` date DEFAULT NULL,
   `idUsuario` int(11) DEFAULT NULL,
+  `direccionPrestador` text,
   PRIMARY KEY (`idPrestador`),
   KEY `idUsuario` (`idUsuario`),
   CONSTRAINT `prestadorservicios_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`)
@@ -61,6 +88,7 @@ CREATE TABLE `solicitante` (
   `direccionSolicitante` text,
   `fechaNacimiento` date DEFAULT NULL,
   `idUsuario` int(11) DEFAULT NULL,
+  `tipo` int(11) DEFAULT NULL,
   PRIMARY KEY (`idSolicitante`),
   KEY `idUsuario` (`idUsuario`),
   CONSTRAINT `solicitante_ibfk_1` FOREIGN KEY (`idUsuario`) REFERENCES `usuario` (`idUsuario`)
@@ -144,4 +172,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-05-10  2:59:57
+-- Dump completed on 2018-05-12  1:00:41
